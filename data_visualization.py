be822523cdd8ef3f50de8d48adc08034dc86b77f
Helen Haveloh <h.havelohh@gmail.com>
2024-10-07 15:42:27 +0200

    first data visualization example

---
+++ data_visualization.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# Beispiel-Daten: Zahlen von 0 bis 9 und ihre Quadrate
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [i**2 for i in x]  # Quadratzahlen
+
+# Liniendiagramm erstellen
+plt.plot(x, y, label='Quadratzahlen')
+
+# Titel und Achsenbeschriftungen hinzufügen
+plt.title('Quadratzahlen von 0 bis 9')
+plt.xlabel('Zahl')
+plt.ylabel('Quadrat')
+
+# Legende anzeigen
+plt.legend()
+
+# Diagramm anzeigen
+plt.show()
