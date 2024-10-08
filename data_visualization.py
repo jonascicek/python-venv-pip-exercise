2b73cf18e621ff0647ec83fe1bdfa5eea5daca10
jonascicek <jonascicek98@gmail.com>
2024-10-08 15:33:38 +0200

    Fertigstellung der HA

--- data_visualization.py
+++ data_visualization.py
@@ -1,19 +1,33 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Beispiel-Daten: Zahlen von 0 bis 9 und ihre Quadrate
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y = [i**2 for i in x]  # Quadratzahlen
 
+# Beispiel-Daten: Zahlen von 0 bis 9 und ihre Kubik
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [i**3 for i in x]  # Kubikzahlen
+
 # Liniendiagramm erstellen
-plt.plot(x, y, label='Quadratzahlen')
+plt.plot(x, y, label='Kubikzahlen')
 
 # Titel und Achsenbeschriftungen hinzufügen
-plt.title('Quadratzahlen von 0 bis 9')
+plt.title('Kubikzahlen von 0 bis 9')
 plt.xlabel('Zahl')
-plt.ylabel('Quadrat')
+plt.ylabel('Kubikzahlen')
 
 # Legende anzeigen
 plt.legend()
 
 # Diagramm anzeigen
 plt.show()
+
+# PNG speichern
+plt.savefig('diagramm.png')
+
+# Pandas Dataframe
+nums = pd.DataFrame(y)
+
+# Pandas to CSV
+nums.to_csv('zahlen.csv', sep='\t', encoding='UTF-8', index=False, header=True)
